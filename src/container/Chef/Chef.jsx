52b3cee03c0cb2f4e7237265f503e3a9957740e2
Rita Uzoma <you@example.com>
2022-11-19 00:32:18 +0100

    Working on the laurels section, have to work on the responsiveness

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import './Chef.css';
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit ametbvjhmjhsabvd badfbdfb
+          </p>
+        </div>
+        <p className="p__opensans">
+          Lorem ipsum dolor sit ametbvjhmjhsabvd Lorem ipsum dolor sit
+          ametbvjhmjhsabvdLorem ipsum dolor sit ametbvjhmjhsabvdLorem ipsum
+          dolor sit ametbvjhmjhsabvdLorem ipsum dolor sit ametbvjhmjhsabvdLorem
+          ipsum dolor sit ametbvjhmjhsabvdLorem ipsum dolor sit
+          ametbvjhmjhsabvdLorem ipsum dolor sit ametbvjhmjhsabvdLorem ipsum
+          dolor sit ametbvjhmjhsabvd
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="signature" />
+      </div>
+    </div>
   </div>
 );
 
