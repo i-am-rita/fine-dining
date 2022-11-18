d474879b6b0108adff55e57dcbe9d61902c1e0a9
Rita Uzoma <you@example.com>
2022-11-18 19:38:18 +0100

    completed the menu section

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -12,8 +12,8 @@ const SpecialMenu = () => (
     </div>
 
     <div className="app__specialMenu-menu">
-      <div className="app__specialMenu-menu_wine flex__center ">
-        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
         <div className="app__specialMenu_menu_items">
           {data.wines.map((wine, index) => (
             <MenuItem
@@ -30,8 +30,8 @@ const SpecialMenu = () => (
         <img src={images.menu} alt="menu-img" />
       </div>
 
-      <div className="app__specialMenu-menu_cocktails flex__center ">
-        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+      <div className="app__specialMenu-menu_cocktails flex__center">
+        <p className="app__specialMenu-menu_heading">Cocktails</p>
         <div className="app__specialMenu_menu_items">
           {data.cocktails.map((cocktail, index) => (
             <MenuItem
